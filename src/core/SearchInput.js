e4e43b79854a2c904f6b688339fca75073143188
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-09-06 17:55:23 +0900

    feat: 페이지 진입 시 autofocus, 마우스 클릭 시 input 값 사라짐

--- src/core/SearchInput.js
+++ src/core/SearchInput.js
@@ -5,9 +5,14 @@ export class SearchInput {
     const $searchInput = document.createElement("input");
     this.$searchInput = $searchInput;
     this.$searchInput.placeholder = "고양이를 검색해보세요 :)";
+    this.$searchInput.autofocus = true;
 
     $searchInput.className = "SearchInput";
     $target.appendChild($searchInput);
+
+    $searchInput.addEventListener("click", () => {
+      $searchInput.value = "";
+    });
 
     $searchInput.addEventListener("keyup", (e) => {
       if (e.keyCode === 13) {

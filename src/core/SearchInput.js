e3324b61b4f16aaf343b382e25f580ed4c5f3bef
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-09-13 15:15:42 +0900

    fix: 최근 키워드 검색 에러 해결, 완성
    - 검색어 입력 시, `this.SearchInput.addEventListener("keyup", (e) => {...})` 부분이 n번씩 실행되는 에러 해결
    - paintKeyword() 추가
    - addKeyword()에서 마지막에 render() 대신 paintKeyword() 호출하는 것으로 수정

--- src/core/SearchInput.js
+++ src/core/SearchInput.js
@@ -6,47 +6,26 @@ export class SearchInput {
     this.keywords = getItem("key") || [];
     this.section = document.createElement("section");
     this.section.className = "search-section";
+
+    this.SearchInput = document.createElement("input");
+    this.SearchInput.placeholder = "고양이를 검색해보세요 :)";
+    this.SearchInput.autofocus = true;
+    this.SearchInput.className = "search-input";
     $target.appendChild(this.section);
     this.render();
   }
 
-  deleteKeyword(keyword) {
-    this.keywords = this.keywords.filter((value) => {
-      return value != keyword;
-    });
-    setItem("key", this.keywords);
-  }
-
   addKeyword(keyword) {
-    // 이미 최근 검색창에 포함되어 있는 검색어는 최근 검색어에 추가 X
     if (this.keywords.includes(keyword)) return;
     if (this.keywords.length == 5) this.keywords.shift();
     this.keywords.push(keyword);
     setItem("key", this.keywords);
-    this.render();
+    this.paintKeyword();
   }
 
-  render() {
+  paintKeyword() {
     this.section.innerHTML = "";
 
-    // 고양이 검색창
-    const SearchInput = document.createElement("input");
-    SearchInput.placeholder = "고양이를 검색해보세요 :)";
-    SearchInput.autofocus = true;
-    SearchInput.className = "search-input";
-
-    SearchInput.addEventListener("click", () => {
-      SearchInput.value = "";
-    });
-
-    SearchInput.addEventListener("keyup", (e) => {
-      if (e.keyCode === 13) {
-        this.onSearch(SearchInput.value);
-        this.addKeyword(SearchInput.value);
-      }
-    });
-
-    // 최근 검색어 창
     const recentKeywords = document.createElement("section");
     recentKeywords.className = "recent-keywords";
     recentKeywords.innerHTML = "";
@@ -59,16 +38,29 @@ export class SearchInput {
       const keywords = document.createElement("div");
       keywords.className = "keyword-list";
       keywords.innerText = keyword;
-      keywords.addEventListener("click", (e) => {
-        if (e.target.className === "keyword-list") {
-          SearchInput.value = keyword;
-          this.onSearch(keyword);
-        }
+      keywords.addEventListener("click", () => {
+        this.onSearch(keyword);
+        this.SearchInput.value = keyword;
       });
       recentKeywords.appendChild(keywords);
     });
 
-    this.section.appendChild(SearchInput);
+    this.section.appendChild(this.SearchInput);
     this.section.appendChild(recentKeywords);
+  }
+
+  render() {
+    this.SearchInput.addEventListener("click", () => {
+      this.SearchInput.value = "";
+    });
+
+    this.SearchInput.addEventListener("keyup", (e) => {
+      if (e.keyCode === 13) {
+        this.onSearch(this.SearchInput.value);
+        this.addKeyword(this.SearchInput.value);
+      }
+    });
+
+    this.paintKeyword();
   }
 }
